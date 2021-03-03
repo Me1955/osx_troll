1398b02cbba44f9a30d6434ec35cd70fd6c2adda
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-03-03 16:30:19 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/P.mp3 --output ~/Desktop/.troll.mp3
 
+#Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 caffeinate &
 afplay ~/Desktop/.troll.mp3 &
 
 end=$((SECONDS+5))
 
 while [ $SECONDS -lt $end ]; do
-	osascript -e 'set volume 3'
+	osascript -e 'set volume 100'
 done
-
-#Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
